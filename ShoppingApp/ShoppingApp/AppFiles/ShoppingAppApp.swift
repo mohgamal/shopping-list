4b462b660f8af9980c366a78df05e8266f43ad63
Mohammed Abdelaty <mohammed.a@azenia.com>
2022-09-08 18:58:32 +0200

    updating app structure

---
+++ ShoppingApp/ShoppingApp/AppFiles/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Mohammed Abdelaty on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
