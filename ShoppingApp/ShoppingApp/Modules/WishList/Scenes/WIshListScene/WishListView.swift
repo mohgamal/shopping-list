2d5fc97dbd77f4517c8b5c24d462afb7e708d642
Mohammed Abdelaty <mohammed.a@azenia.com>
2022-09-11 11:52:19 +0200

    adding wish list view and view model

--- ShoppingApp/ShoppingApp/Modules/WishList/Scenes/WIshListScene/WishListView.swift
+++ ShoppingApp/ShoppingApp/Modules/WishList/Scenes/WIshListScene/WishListView.swift
@@ -10,26 +10,55 @@ import SwiftUI
 
 struct WishListView: View {
     @Environment(\.presentationMode) var presentationMode
+    @ObservedObject public var vm: WishListViewModel = WishListViewModel(wishListManager: WishListManager())
+
 
     var body: some View {
-        ZStack {
-            VStack{
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Image(systemName: "xmark")
-                            .resizable()
-                            .foregroundColor(.black)
-                            .frame(width: 20, height:20)
-                            .padding()
+        NavigationView {
+            ScrollView {
+                VStack {
+                    ForEach(vm.wishList, id: \.id) { product in
+                        VStack {
+                            WishListProductCell(product: product)
+                            Button(action: {
+                                vm.removeProductFromWIshList(id: product.id ?? "")
+                            }, label: {
 
+                                Image(systemName:  "trash")
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                                Text("Remove")
+                                    .underline()
+                            })
+                            .foregroundColor(.black)
+                            Divider()
+                        }
                     }
                 }
+                .toolbar {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }, label: {
+                        Image(systemName:  "xmark")
+                            .resizable()
+                            .frame(width: 25, height:25)
+                    })
 
-                Spacer()
+                    .foregroundColor(.black)
+                }
+                .navigationBarTitle( "WISHLIST (\(vm.wishList.count))", displayMode: .inline)
+                .background(NavigationConfigurator { nc in
+                    nc.navigationBar.titleTextAttributes = [.foregroundColor : UIColor.black]
+                })
             }
         }
+        .navigationViewStyle(StackNavigationViewStyle())
+        .onAppear {
+            reloadData()
+        }
+    }
+
+    private func reloadData() {
+        self.vm.getWishListProducts()
     }
 }
--- ShoppingApp/ShoppingApp/Modules/WishList/Scenes/WIshListScene/WishListViewModel.swift
+++ ShoppingApp/ShoppingApp/Modules/WishList/Scenes/WIshListScene/WishListViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class WishListViewModel: ObservableObject {
+    @Published var wishList: [ProductModel] = []
+    let wishListManager: WishListManager
+
+    init (wishListManager: WishListManager) {
+        self.wishListManager = wishListManager
+    }
+
+    func getWishListProducts() {
+        wishList = wishListManager.getList()
+        self.objectWillChange.send()
+    }
+
+    func removeProductFromWIshList(id: String) {
+        wishList = wishListManager.remove(id: id)
+        self.objectWillChange.send()
+    }
+}
